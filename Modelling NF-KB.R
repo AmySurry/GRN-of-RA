82ac273a7eca4a87ccbcbee2adcf149c58a614d2
AmySurry <amy@surry.dk>
2024-10-31 11:18:25 +0100

    Simple NF-KB - plot

--- Modelling NF-KB.R
+++ Modelling NF-KB.R
@@ -4,7 +4,7 @@ rm(list = ls())
 library(deSolve)
 library(ggplot2)
 
-# T/F statement for activating the parthway
+# T/F statement for activating the pathway
 activation = T
 
 # Differential equations
@@ -22,11 +22,12 @@ NF_KB <- function(time,state,parameters){
   b4 <- parameters["beta4"]
   
   if(activation == T){
-    R <- 1
+    R <- 1 # Receptor activation
   }else{
     R <- 0
   }
   
+  # The differential equations
   dA <- R*b1-a1*A
   dI <- A*b3-a2*I
   dP <- A*b2-b4*I-a3*P
@@ -39,15 +40,17 @@ NF_KB <- function(time,state,parameters){
 # Initial condition 
 x0 <- c(A = 0, I = 0, P = 0) 
 
-beta1 <- log(2)
-beta2 <- log(2)
-beta3 <- log(2)
-beta4 <- log(2)
-alpha1 <- 0.5
-alpha2 <- 0.5
-alpha3 <- 0.5
+# Parameter values from Jaruszewicz-Błońska et al. (2023)
+alpha1 <- 0.00145 # s^-1 Spontaneous inactivation of IKK complex
+alpha2 <- 0.0763 # s^-1 IκBα degradation induced by IKK complex
+alpha3 <- 0.000372+0.000106+0.0428  # s^-1 IκBα synthesis and degradation of IκBα mRNA
 
-# Adjust parameters to allow comparison
+beta1 <- 0.00195 # s^-1 Activation of IKK complex
+beta2 <- 0.0946 # s^-1 IKK (IκBα|NF-κB) association
+beta3 <- 0.0946 # s^-1 IKK (IκBα|NF-κB) association
+beta4 <- 0.000595 # s^-1 IκBα nuclear import leading to... (repression of NF-κB)
+
+# Parameter vector
 parameters <- c(alpha1 = alpha1, 
                 alpha2 = alpha2, 
                 alpha3 = alpha3,
@@ -57,7 +60,7 @@ parameters <- c(alpha1 = alpha1,
                 beta4 = beta4)
 
 # Time sequence
-times <- seq(0, 100, by = 1) 
+times <- seq(0, 1500, by = 1) 
 
 # Solve ODEs
 res_NF_KB <- ode(x0, times, NF_KB, parameters) 
@@ -66,11 +69,14 @@ res_NF_KB <- as.data.frame(res_NF_KB)
 
 # Plotting the results
 ggplot(res_NF_KB, aes(x = time)) +
-  geom_line(aes(y = A, color = "A")) +
-  geom_line(aes(y = I, color = "I")) +
-  geom_line(aes(y = P, color = "P")) +
+  geom_line(aes(y = A, color = "IKK complex")) +
+  geom_line(aes(y = I, color = "IKBa")) +
+  geom_line(aes(y = P, color = "NF-KB")) +
   labs( x = "Time", y = "Concentration") +
-  scale_color_manual(name = " ", values = c("A" = "blue", "I" = "orange", "P" = "green")) +
+  scale_color_manual(name = " ", 
+                     values = c("IKK complex" = "blue", 
+                                            "IKBa" = "orange", 
+                                            "NF-KB" = "green")) +
   theme_minimal()
 
 
